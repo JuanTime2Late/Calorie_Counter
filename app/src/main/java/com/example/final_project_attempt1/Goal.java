657b749b66fd59388bd24dc7ed71d7a9a45aed0e
JuanTime2Late <juan.vera05@utrgv.edu>
2023-04-23 14:52:56 -0500

    wHATEVER THERE

--- app/src/main/java/com/example/final_project_attempt1/Goal.java
+++ app/src/main/java/com/example/final_project_attempt1/Goal.java
@@ -37,27 +37,28 @@ public class Goal extends AppCompatActivity {
         //Date TextView
         TextView textViewDate = findViewById(R.id.text_view_date);
         textViewDate.setText(currentDate);
+
         /////////New goal Submission\\\\\\\\\
-        edt1 = (EditText) findViewById(R.id.editTextNumber);
-        btn1 = (Button) findViewById(R.id.Submit_goal);
-        tv1 = (TextView) findViewById(R.id.CurrGoal);
+        edt1 = (EditText) findViewById(R.id.editTextNumber); //textbox to enter new calorie goal
+        btn1 = (Button) findViewById(R.id.Submit_goal); //Button to submit goal
+        tv1 = (TextView) findViewById(R.id.CurrGoal); //Show the current goal received from main.
         //Goal Display
-        tv1.setText(getIntent().getStringExtra("message"));
+        tv1.setText(getIntent().getStringExtra("message")); //receive from main new goal.
 
 
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+        btn1.setOnClickListener(new View.OnClickListener() { //Submit button pressed.
             @Override
-            public void onClick(View view) {
-                String input = edt1.getText().toString();
-                if(!TextUtils.isEmpty(input)) {
-                    tv1.setText(input);
+            public void onClick(View view) { //When button is pressed
+                String input = edt1.getText().toString(); //take input from new goal
+                if(!TextUtils.isEmpty(input)) { //if input is not empty
+                    tv1.setText(input); //set current goal to the new number
                     Intent intent = new Intent(Goal.this, MainActivity.class);
-                    intent.putExtra("message", input);
+                    intent.putExtra("message", input); //send to the goal new number
                     startActivity(intent);
                 }
-                else{
-                        Toast.makeText(Goal.this, "Please Enter an Amount.",
+                else{ //If it is empty and the button is pressed ask the user for an input again.
+                        Toast.makeText(Goal.this, "Please enter an amount.",
                                 Toast.LENGTH_SHORT).show();
                 }
             }
--- app/src/main/java/com/example/final_project_attempt1/MainActivity.java
+++ app/src/main/java/com/example/final_project_attempt1/MainActivity.java
@@ -13,9 +13,9 @@ import java.text.DateFormat;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tv1, tv2, tv3;
-    EditText food, cals;
-    Holder mDatabaseHelper;
+    TextView tv1, tv2, tv3; //Declaring buttons
+    EditText food, cals; //Declaring input boxes
+    Holder mDatabaseHelper; //Declaring Database class object
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,72 +28,70 @@ public class MainActivity extends AppCompatActivity {
         //Date TextView
         TextView textViewDate = findViewById(R.id.text_view_date);
         textViewDate.setText(currentDate);
+
         /////////////Goal Display\\\\\\\\\\\\\\\\
-        tv3 = findViewById(R.id.total);
-        tv2 = findViewById(R.id.textView7);
-        tv1 = findViewById(R.id.textView6);
-        if(getIntent().getStringExtra("message") != ""){
+        tv3 = findViewById(R.id.total); //TextView variable for Total calories.
+        tv2 = findViewById(R.id.textView7); //Number of Eaten calories.
+        tv1 = findViewById(R.id.textView6); //Number of Calories for goal.
+        if(getIntent().getStringExtra("message") != ""){ //This receives the goal amount from Goal.java
             tv1.setText(getIntent().getStringExtra("message"));
         }
-
-         //receive from goal class
         /////////////Goal Button\\\\\\\\\\\\\\\\
-        Button btn1 = findViewById(R.id.GoalButton);
-        btn1.setOnClickListener(v -> openGoal());
+        Button btn1 = findViewById(R.id.GoalButton); //Btn1 is for Change Goal
+        btn1.setOnClickListener(v -> openGoal()); //On click it will go to openGoal(see below)
 
         /////////////Enter Food and Calories\\\\\\\\\\\\\\
-        cals = findViewById(R.id.Calories);
-        food = findViewById(R.id.Food);
-        Button btn2 = findViewById(R.id.submit1);
-        Button btn3 = findViewById(R.id.button2);
-        mDatabaseHelper = new Holder(this);
+        cals = findViewById(R.id.Calories); //Variable cals finds input from Enter Calories box
+        food = findViewById(R.id.Food); //Variable food find input from Enter food box.
+        Button btn2 = findViewById(R.id.submit1); //Button for submitting food and calories
+        Button btn3 = findViewById(R.id.button2); //Button for Today's Calories
+        mDatabaseHelper = new Holder(this); // object initialized with new Database class obj
 
-        btn2.setOnClickListener(view -> {
-            String newEntry = cals.getText().toString();
-            if(cals.length() != 0 && food.length() != 0) {
+        btn2.setOnClickListener(view -> { //On click it will do the next
+            String newEntry = cals.getText().toString(); // Declaring variable to hold calorie input
+            if(cals.length() != 0 && food.length() != 0) { //
                 if(!tv1.getText().toString().isEmpty() && !(tv2.getText().toString().isEmpty())) {
                     int eat = Integer.parseInt(tv2.getText().toString());
                     int cal = Integer.parseInt(cals.getText().toString());
-                    int var1 = eat + cal;
-                    tv2.setText(Integer.toString(var1));
+                    int var1 = eat + cal; //adds calories to existing Eaten calories
+                    tv2.setText(Integer.toString(var1));//sets Eaten to the total
                     int num = Integer.parseInt(tv1.getText().toString());
-                    int var = num - var1;
-                    tv3.setText(Integer.toString(var));
+                    int var = num - var1; //Subtracts the Eaten total from the goal(can go negative)
+                    tv3.setText(Integer.toString(var)); //sets Total subtraction to the remaining.
                 }
-                AddData(newEntry);
-                cals.setText("");
-            } else { toastMessage("Either Food or Calories is empty.");}
-
-
+                AddData(newEntry); //adds to the list the calorie amount
+                cals.setText(""); //after input it makes the edittext empty again.
+            } else { toastMessage("Either Food or Calories is empty.");} //else tell the user one
+                                                                        //of the fields is empty.
         });
 
-        btn3.setOnClickListener(view -> {
+        btn3.setOnClickListener(view -> { //Opens today's Calories activity (today.java and xml)
             Intent intent = new Intent(MainActivity.this, today.class);
                 startActivity(intent);
         });
     }
 
-    public void AddData(String entry) {
-        boolean insertData = mDatabaseHelper.addData(entry);
-
-        if(insertData) {
-            toastMessage("Data Successfully Inserted!");
+    public void AddData(String entry) { //Adds the calories to the list
+        boolean insertData = mDatabaseHelper.addData(entry); //This will insert data to list
+        //and make sure it is done correctly through boolean
+        if(insertData) { //If the data is entered correctly
+            toastMessage("Data Successfully Inserted!"); //Display to the user success.
         }
         else {
-            toastMessage("smn wrong");
+            toastMessage("smn wrong"); //Display to the user that data wasn't entered correctly.
         }
     }
 
-    private void toastMessage(String s) {
+    private void toastMessage(String s) { //Method so I dont have to retype it all the time.
         Toast.makeText(this,s , Toast.LENGTH_SHORT).show();
     }
 
-    public void openGoal(){
-        String input = tv1.getText().toString();
-        Intent intent = new Intent(this, Goal.class);
+    public void openGoal(){ //Function to open Goal activity
+        String input = tv1.getText().toString(); //Input takes the string from goal calories
+        Intent intent = new Intent(this, Goal.class); //receives from goal current goal
         startActivity(intent);
-        Intent intent2 =new Intent(MainActivity.this, Goal.class);
-        intent2.putExtra("message", input);
+        Intent intent2 =new Intent(MainActivity.this, Goal.class); //sends current goal
+        intent2.putExtra("message", input);                               //to goal activity
         startActivity(intent2);
-    } //this hs==fdfad affvaeevawfavwfaevfa
+    }
 }
